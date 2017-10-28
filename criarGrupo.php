43fb1e7c75991bf2fb5c3f8f0bea56c728cbc0e8
Marcelo Azevedo <marcelobernardy@gmail.com>
2017-10-28 18:09:28 -0200

    Lugares include

--- criarGrupo.php
+++ criarGrupo.php
@@ -12,22 +12,7 @@
 </head>
 <body>
   <?php include("header.php"); ?>
-      <!--
-    </div>
-    <div class="nav-content">
-      <ul class="tabs tabs-transparent">
-        <li class="tab"><a href="#test1">Test 1</a></li>
-        <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-        <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
-        <li class="tab"><a href="#test4">Test 4</a></li>
-      </ul>
-    </div>
-  </nav>
-  <div id="test1" class="col s12">Test 1</div>
-  <div id="test2" class="col s12">Test 2</div>
-  <div id="test3" class="col s12">Test 3</div>
-  <div id="test4" class="col s12">Test 4</div>
--->
+    
 <main>
 <div class="container">
   <div class="row">
@@ -36,11 +21,12 @@
         <div class="col s12">
           <div class="form-group">
             <div class="col s8 offset-l2">
+
               <input class="file-upload-input-image" onchange="readURL(this);" type="file" accept="image/*">
             </div>
             <div class="col s8 offset-l2">
             
-              <input class="form-control" type="text" name="user" placeholder="Digite um nome para a sua equipe">
+              <input class="form-control" type="text" name="user" id="nomeEquipe" placeholder="Digite um nome para a sua equipe">
             </div>
           </div>
           
