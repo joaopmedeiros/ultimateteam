c9e28c360286e8505f067821386a25f59a7dff02
Gabriel Brunichaki <gabriel.brunichaki@acad.pucrs.br>
2017-10-29 00:41:09 -0200

    Header e footer ajustados

--- header.php
+++ header.php
@@ -1,36 +1,40 @@
-<header>
+<?php require_once 'controllers/checkSession.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+		<title><?php echo $title; ?></title>
 
-<nav "nav-extended">
-<div class="nav-wrapper teal lighten-2">
-  <a href="#!" class="brand-logo">Ultimate Team</a>
-  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">+</i></a>
-  <ul class="right hide-on-med-and-down">
-  <li><a href="dashboard.php">Dashboard</a></li>
-  <li><a href="#">Perfil</a></li>
-  <li><a href="grupos.php">Grupos</a></li>
-  <li><a href="confrontoDireto.php">Confronto Direto</a></li>
-  <li><a href="#">Configurações</a></li>
-  <li><a href="logout.php">Logoff</a></li>
-  </ul>
-  <ul class="side-nav" id="mobile-demo">
-  
-  <li ><img src="img/calebe.jpg" class="circle" width="20%">
-  
-  <li><a href="#">Calebe</a></li>
-  
-  <li><a href="dashboard.php">Dashboard</a></li>
-  <li><a href="#">Perfil</a></li>
-  <li><a href="grupos.php">Grupos</a></li>
-  <li><a href="confrontoDireto.php">Confronto Direto</a></li>
-  <li><a href="#">Configurações</a></li>
-  <li><a href="logout.php">Logoff</a></li>
-  </ul>
-  </div>
-</nav>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="css/materialize.css" media="screen,projection"/>
+		<link rel="stylesheet" href="css/style.css" media="screen,projection"/>		
+	</head>
 
-
-  <script>
-    $(".button-collapse").sideNav();
-  </script>
-
-</header>
+	<body>
+		<header>
+			<nav "nav-extended">
+				<div class="nav-wrapper teal lighten-2">
+					<a href="#!" class="brand-logo">Ultimate Team</a>
+					<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+					<ul class="right hide-on-med-and-down">
+						<li><a href="dashboard.php">Dashboard</a></li>
+						<li><a href="#">Perfil</a></li>
+						<li><a href="grupos.php">Grupos</a></li>
+						<li><a href="confrontoDireto.php">Confronto Direto</a></li>
+						<li><a href="#">Configurações</a></li>
+						<li><a href="logout.php">Logoff</a></li>
+					</ul>
+					<ul class="side-nav" id="mobile-demo">
+						<li ><img src="img/calebe.jpg" class="circle" width="20%">
+						<li><a href="#">Calebe</a></li>
+						<li><a href="dashboard.php">Dashboard</a></li>
+						<li><a href="#">Perfil</a></li>
+						<li><a href="grupos.php">Grupos</a></li>
+						<li><a href="confrontoDireto.php">Confronto Direto</a></li>
+						<li><a href="#">Configurações</a></li>
+						<li><a href="logout.php">Logoff</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
