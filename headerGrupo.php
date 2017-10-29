5c9bd4ef9531e96c0da6ceafa97603fe8a58713e
Marcelo Azevedo <marcelobernardy@gmail.com>
2017-10-28 23:26:09 -0200

    header

--- headerGrupo.php
+++ headerGrupo.php
@@ -1,8 +1,15 @@
+
+
+<?php
+require_once 'class/Grupo.class.php';
+$cGrupo = new Grupo();
+?>
+
 <header>
 <nav class="nav-extended card-panel teal lighten-2">
 <div class="nav-wrapper">
-  <a href="#" class="brand-logo">Logo</a>
-  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+  <a href="#" class="brand-logo"><?php $cGrupo->getNome(); ?></a>
+  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">+</i></a>
   <ul id="nav-mobile" class="right hide-on-med-and-down">
   <li><a href="dashboard.php">Dashboard</a></li>
   <li><a href="#">Perfil</a></li>
@@ -27,17 +34,17 @@
 </div>
 <div class="nav-content">
   <ul class="tabs tabs-transparent">
-    <li class="tab"><a href="#test1">Test 1</a></li>
-    <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-    <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
-    <li class="tab"><a href="#test4">Test 4</a></li>
+    <li class="tab"><a href="#test1">Partida</a></li>
+    <li class="tab"><a herf="#test2" >Classificação</a></li>
+    <li class="tab"><a href="#test3">Valendo</a></li>
+    
   </ul>
 </div>
 </nav>
-<div id="test1" class="col s12">Test 1</div>
-<div id="test2" class="col s12">Test 2</div>
-<div id="test3" class="col s12">Test 3</div>
-<div id="test4" class="col s12">Test 4</div>
+<div id="test1" class="col s12"><?php include("partida.php"); ?></div>
+<div id="test2" class="col s12"><?php include("classificacao.php"); ?></div>
+<div id="test3" class="col s12"><?php include("valendo.php"); ?></div>
+
   <script>
     $(".button-collapse").sideNav();
   </script>
